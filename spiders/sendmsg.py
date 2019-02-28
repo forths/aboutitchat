aeeca0e01a337a8caaafbbf5cdc5009949bd5475
guanyou <gemfield@deepait.com>
2019-02-28 10:54:58 +0800

    changed some

--- spiders/sendmsg.py
+++ spiders/sendmsg.py
@@ -1,5 +1,16 @@
 import itchat
 
-itchat.auto_login()
+itchat.auto_login(enableCmdQR=2,hotReload=True)
 
-itchat.send('Hello, filehelper', toUserName='filehelper')
+def send_onegroup(msg,gname):
+    # rooms = itchat.get_chatrooms(update=True)
+    rooms = itchat.search_chatrooms(gname)
+    if rooms is not None:
+        for i in range(1):
+            username = rooms[0]['UserName']
+            itchat.send(msg,toUserName=username)
+            itchat.send_image("@e610ba4cf0b2902929e1c529b706c4f3b5936208972f98707442a88be01253d4.png",toUserName=username)
+    else:
+        print("None group found.")
+if __name__ == "__main__":
+    send_onegroup(u"Hello world,I am send by program sendmsg.py!","testmsg")
--- spiders/wordCloud.py
+++ spiders/wordCloud.py
@@ -1,5 +1,6 @@
-#-*- coding：utf-8 -*-
-#导入需要使用的相关模块
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 import itchat
 import re
 import jieba
@@ -28,27 +29,23 @@ def getnum_alpha_china_words(oprwords):
                 tmp += ''
         res.append(tmp)
     return ' '.join(res)
-#登录方法，会弹出登录二维码，用微信扫描登录
+
 itchat.auto_login()
 
-#关于所有微信还有的资料信息都封装在这个方法里
 friends = itchat.get_friends(update=True)[0:]
 user = friends[0]["UserName"]
-#获取好友签名信息
+
 siglist = []
 
-#遍历好友信息
 for i in friends:
-    #过滤信息
     signature = i['Signature'].strip().replace('span','').replace('class','').replace('emoji','')
     rep = re.compile('1f\d+\w*|[<>/=]')
     signature = rep.sub(' ',signature)
     signature = getnum_alpha_china_words(signature)
     siglist.append(signature)
-#所有签名信息封装在text中
+
 text = ' '.join(siglist)
 
-#写入本地文件
 textfile = open(user+'.txt','w',encoding="utf-8")
 textfile.write(text)
 textfile.close()
@@ -65,25 +62,22 @@ for i in word_space_split:
 cres = Counter(res)
 print(len(cres))
 print(cres)
-#画图
 coloring = plt.imread('./haha.jpg')
-#设置词云相关属性
+
 my_wordcloud = WordCloud(background_color='white',
                         max_words=200,
                         mask=coloring,
                         max_font_size=150,
                         random_state=42,
                         font_path='‪C:\Windows\Fonts\simkai.ttf').fit_words(cres)
-                        # font_path='‪C:\Windows\Fonts\simkai.ttf').generate_from_text(word_space_split)
-
 
 image_colors = ImageColorGenerator(coloring)
-# 保存图片
+
 d = path.dirname(__file__)
 my_wordcloud.recolor(color_func=image_colors)
 my_wordcloud.to_file(path.join(d, user+".png"))
 itchat.send_image(user+".png",toUserName='filehelper')
-#显示词云
+
 plt.imshow(my_wordcloud)
 plt.axis('off')
 plt.show()
